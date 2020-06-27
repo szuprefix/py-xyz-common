20766c46a1b07161775826aaea7f237ef210c036
denishuang <szuprefix@126.com>
2020-06-27 14:15:37 +0800

    all

--- xyz_common/apis.py
+++ xyz_common/apis.py
@@ -7,25 +7,34 @@ from xyz_restful.decorators import register
 __author__ = 'denishuang'
 
 
-from django.contrib.contenttypes.models  import ContentType
 
+from django.contrib.contenttypes import models as ctmodels
 
 @register()
-class ContenttypeViewSet(viewsets.ModelViewSet):
-    queryset = ContentType.objects.all().order_by('app_label')
+class ContenttypeViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ctmodels.ContentType.objects.all().order_by('app_label')
     serializer_class = serializers.ContenttypeSerializer
+    permission_classes = []
     search_fields = ('app_label', 'model')
     filter_fields = {
         'id': ['exact', 'in']
     }
 
     def filter_queryset(self, queryset):
+        if self.action == 'all':
+            return self.queryset
         from xyz_auth.helper import get_user_model_permissions
         from django.db.models import Q
-        mps = get_user_model_permissions(self.request.user)
+        user = self.request.user
+        qset = super(ContenttypeViewSet, self).filter_queryset(queryset)
+        mps = get_user_model_permissions(user)
         ms = [m.split('.') for m in mps.keys()]
         lookup = None
         for m in ms:
             q = Q(**dict(app_label=m[0], model=m[1]))
             lookup = (lookup | q) if lookup else q
-        return lookup and queryset.filter(lookup) or queryset
+        return qset.filter(lookup) if lookup else qset
+
+    @decorators.list_route(['GET'])
+    def all(self, request):
+        return self.list(request)
